d94a5b272aaac20303d08706af8d94548e6ab85e
sonu-codes <114028056+sonu-codes@users.noreply.github.com>
2023-11-07 21:48:23 +0530

    Add files via upload

---
+++ run.py
@@ -0,0 +1,21 @@
+import streamlit as st
+st.info("This link will expire on 13 Nov, 2023 11:59pm")
+st.header("Link Verified 	:white_check_mark:")
+st.subheader("Roll No: 22F2000835")
+
+
+st.write("""
+Note:\n
+If you are experiencing any issues with viewing your results, please refresh the page or try again later.\n
+For any technical assistance or questions related to your results, 'CONTACT US'.
+   
+""")
+st.subheader("Contact Us")
+st.write("""
+IITM BS Degree Office,\n 
+3rd Floor, ICSR Building,\n
+IIT Madras, Chennai - 600036.   
+Email : support@study.iitm.ac.in
+""")
+
+st.write("""Note: Please sent email from your registered college email address.""")
